ac38e51aceda3ad865a4c19e082e19c06c8fb3fb
Vered Rekanati <veredrec@gmail.com>
2017-12-05 07:22:51 +0200

    fix age message and create rel verification

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 var submit = document.getElementsByTagName('button')[1];
+var messageAge = document.createTextNode('h2');
+var messageRel = document.createTextNode('h2');
 
-// var userInput = {};
+function createMessageAge(message) {
+  messageAge.textContent = message;
+  document.body.appendChild(messageAge);
+}
+function createMessageRel(message) {
+  messageRel.textContent = message;
+  document.body.appendChild(messageRel);
+}
+
+function verifyAge(age) {
+  if (age === '') {
+    createMessageAge(
+      'The filed "Age" is required. Please provide a valid age. '
+    );
+  } else if (age <= 0) {
+    createMessageAge('Age cannot be lower than 0... ');
+  }
+}
+
+function verifyRel(rel) {
+  if (rel === '') {
+    createMessageRel(
+      'The field "Relationship" is required. Please choose one of the options provided.'
+    );
+  }
+}
 
 submit.addEventListener('click', function(event) {
+  messageAge.textContent = '';
+  messageRel.textContent = '';
   console.log('submit');
   event.preventDefault();
   var age = document.getElementsByTagName('input')[0].value;
-  createMessage(age);
+  var rel = document.getElementsByTagName('select')[0].value;
+  verifyAge(age);
+  verifyRel(rel);
 });
-
-function createMessage(age) {
-  var div = document.createElement('div');
-  document.body.appendChild(div);
-  var message = document.createTextNode('h1');
-  message.innerText = 'age';
-  div.appendChild(message);
-}
-
-// function verifyAge(age) {
-//   if (age === '') {
-//     createMessage('required field');
-//   } else if (age <= 0) {
-//     createMessage('age lower than 0');
-//   }
-// }
